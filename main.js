31598cccb406c159bd80f02bf84c5e7ed5e49df0
Mohammed Fakih <mhd.fakih@gmail.com>
2023-07-07 12:00:07 +0300

    prepared error message under input field

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
 "use strict";
 
+const calcButton = document.querySelector("#calc-btn");
+// get all 3 input fields
 const dayInput = document.querySelector("#day");
 const monthInput = document.querySelector("#month");
 const yearInput = document.querySelector("#year");
+// get all 3 labels
+const dayLabel = document.querySelector("#label-day");
+const monthLabel = document.querySelector("#label-month");
+const yearLabel = document.querySelector("#label-year");
+
+// get results elements where numbers will be shown
 const resultYears = document.querySelector("#result-years");
 const resultMonths = document.querySelector("#result-months");
 const resultDays = document.querySelector("#result-days");
+// code to get current date ex. 7-7-2023
 const dateObject = new Date();
 let currentYear = dateObject.getFullYear();
-let dayMonthCheck = undefined;
+let currentMonth = dateObject.getMonth() + 1;
+let currentDay = dateObject.getDate();
 
-dayInput.addEventListener("change", function () {
+calcButton.addEventListener("click", function () {
   const dayInputValue = document.querySelector("#day").value;
-  if (+dayInputValue > 31 || +dayInputValue <= 0 || dayInputValue === isNaN) {
-    alert(`${dayInputValue} is not valid day`);
-  } else {
-    resultDays.textContent = dayInputValue;
-    dayMonthCheck = +dayInputValue;
-    alert(dayMonthCheck);
-  }
-});
-
-monthInput.addEventListener("change", function () {
   const monthInputValue = document.querySelector("#month").value;
+  const yearInputValue = document.querySelector("#year").value;
+
+  // validate all dates entered by the user
+
+  if (dayInputValue == "") {
+    showDayError();
+  } else if (
+    +dayInputValue > 31 ||
+    +dayInputValue <= 0 ||
+    dayInputValue === NaN
+  ) {
+    showDayError();
+  }
+
   if (+monthInputValue > 12 || +monthInputValue <= 0) {
     alert("not valid month");
-  } else {
-    resultMonths.textContent = monthInputValue;
-    alert(dayMonthCheck + "from winthin month");
+  }
+  if (+yearInputValue > currentYear) {
+    alert("not valid year");
   }
 });
 
-yearInput.addEventListener("change", function () {
-  const yearInputValue = document.querySelector("#year").value;
-  if (+yearInputValue > currentYear) {
-    alert("not valid year");
-  } else {
-    resultYears.textContent = currentYear - yearInputValue;
-  }
-});
+// function to change day input and label element when value is not valid
+
+function showDayError() {
+  dayInput.classList.add("invalid");
+  dayLabel.classList.add("invalid-label");
+}
